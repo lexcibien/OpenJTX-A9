b8666396540e548529dbe45055236e391d2bfc4a
Alex Luiz <alexci@hotmail.com>
2026-06-17 23:29:05 -0300

    small test on screen

--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -1,4 +1,4 @@
-#include "MainWindow.h"
+#include "mainWindow.h"
 #include "ui_MainWindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -6,9 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->continuityText->setText("Fechado");
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+MainWindow::~MainWindow() { delete ui; }
